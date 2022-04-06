d21b7a741f28ad876d98fdf877874488f99f0118
houssemGit <houssem.eleuch69@gmail.com>
2022-04-06 19:02:01 +0200

    EP-addEurekaServer

---
+++ condidate-service/src/main/java/com/esprit/candidate/CandidateServiceApplication.java
@@ -0,0 +1,16 @@
+package com.esprit.candidate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CandidateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidateServiceApplication.class, args);
+	}
+
+}
