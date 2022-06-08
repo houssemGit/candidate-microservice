20e80488ac8570d23a6a624e2c3cdbccc82bf211
Houssem Zerai <houssem.zeraii@digit-u.com>
2022-06-08 15:24:47 +0100

    latest version

--- src/main/java/com/esprit/candidate/exception/CandidateNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.esprit.candidate.exception;
-
-import java.util.UUID;
-
-public class CandidateNotFoundException extends Throwable {
-    public CandidateNotFoundException(UUID id) {
-        super("Cannot found candidate number [" + id + "]");
-    }
-}
