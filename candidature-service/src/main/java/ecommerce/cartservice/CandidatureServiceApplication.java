727ae38612187f33dec29d7f82b8db49acdb85e0
houssemGit <houssem.eleuch69@gmail.com>
2022-06-15 08:51:10 +0200

    add log4j

--- candidature-service/src/main/java/ecommerce/cartservice/CandidatureServiceApplication.java
+++ candidature-service/src/main/java/ecommerce/cartservice/CandidatureServiceApplication.java
@@ -3,12 +3,18 @@ package ecommerce.cartservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class CandidatureServiceApplication {
 
+	private static final Logger log = LogManager.getLogger(CandidatureServiceApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(CandidatureServiceApplication.class, args);
+		log.info("application started");
+		log.error("application started");
 	}
 }
